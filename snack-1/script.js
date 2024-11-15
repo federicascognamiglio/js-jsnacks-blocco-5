5d47e6be586501e907cd33ad0a19bca337a71c03
federicascognamiglio <scognamigliofede99@gmail.com>
2024-11-15 16:01:51 +0100

    feat: snack 1 completed

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+1.
+// names.forEach((curName) => console.log(curName));
+
+2.
+const newNames = names.map((curName) => curName);
+console.log(newNames);
